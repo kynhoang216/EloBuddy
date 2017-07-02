cdd196ca52818b6d792a95a90dd7e58d64fe9032
kynhoang216 <kynhoang.216@gmail.com>
2017-07-02 02:27:39 -0700

    Update SpellDatabase.cs

--- EzEvade/Spells/SpellDatabase.cs
+++ EzEvade/Spells/SpellDatabase.cs
@@ -569,7 +569,7 @@ namespace ezEvade
                 angle = 10,
                 charName = "Cassiopeia",
                 dangerlevel = 1,
-                name = "Miasama",
+                name = "CassiopeiaW",
                 missileName = "CassiopeiaWMissile",
                 projectileSpeed = 3000,
                 radius = 195,
@@ -579,7 +579,7 @@ namespace ezEvade
                 spellKey = SpellSlot.W,
                 spellName = "CassiopeiaW",
                 spellType = SpellType.Circular,
-                trapTroyName = "cassiopeia_base_w_wcircle_tar_" + Situation.EmitterTeam() + ".troy",
+                //trapTroyName = "cassiopeia_base_w_wcircle_tar_" + Situation.EmitterTeam() + ".troy",
                 updatePosition = false,
                 hasTrap = true
             });
