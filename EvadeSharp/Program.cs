3ae3001c3db751636c78c5eeef1084dc72452577
kynhoang216 <kynhoang.216@gmail.com>
2017-08-07 22:22:05 +0700

    Update Program.cs

--- EvadeSharp/Program.cs
+++ EvadeSharp/Program.cs
@@ -503,7 +503,7 @@ namespace Evade
                 return;
             }
 
-            if (Utility.PlayerWindingUp && !Orbwalker.IsAutoAttacking)
+            if (Player.Instance.Spellbook.IsAutoAttacking && !Orbwalker.IsAutoAttacking)
             {
                 Evading = true;
                 return;
